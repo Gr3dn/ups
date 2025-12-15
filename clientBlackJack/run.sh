bb405cc93c87d9e65a26aa87415290ba5d3ef1f0
Gr3dn <fiv.pypok@gmail.com>
2025-12-15 17:58:57 +0100

    Fix Java17 run

--- clientBlackJack/run.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# Папка скрипта
-DIR="$(cd -- "$(dirname "$0")" >/dev/null 2>&1 && pwd)"
-CMD="java --module-path \"$DIR/target/lib\" --add-modules=javafx.controls -jar \"$DIR/target/blackjack-game-1.0-SNAPSHOT.jar\""
-echo "$CMD"
-eval $CMD
--- server/build/main.d
+++
@@ -1,3 +0,0 @@
-build/main.o: src/main.c include/server.h include/game.h
-include/server.h:
-include/game.h:
